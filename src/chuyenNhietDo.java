944a17fcfedc9a3fb4c58a7897cceee962399a68
hungnv <hungnv0902@gmail.com>
2019-05-13 11:19:46 +0700

    add file java

---
+++ src/chuyenNhietDo.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class chuyenNhietDo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int chose;
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            chose = input.nextInt();
+
+            switch (chose) {
+                case 1:
+                    System.out.println("Enter fahrenheit: ");
+                    fahrenheit = input.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter Celsius: ");
+                    celsius = input.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (chose != 0);
+    }
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+
+    }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+    }
